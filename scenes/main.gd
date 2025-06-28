ca54778f089241d5b6bf916eedbc2a6c46f4a29d
Andrew Davis <jonandrewdavis@gmail.com>
2025-06-27 23:02:53 -0400

    environment block out

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,8 @@
 extends Node3D
 
+@onready var main_menu = $MainMenu
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and main_menu.visible:
+		main_menu.hide()
+		
