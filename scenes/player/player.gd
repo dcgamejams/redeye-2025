6b3a5c4fcf01c7e3472ec2f92b2a26da0290284c
Yakobh <122582481+Yakobh@users.noreply.github.com>
2025-06-27 23:57:23 -0400

    feat: initial char controller block out

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,22 +1,24 @@
 extends CharacterBody3D
 
+@export var acceleration = 5
+@export var top_speed = 10
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var speed = 0
+var flight_direction = Vector3(1, 0, 0)
+var flying = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if not flying:
+		if Input.is_action_just_pressed("launch"):
+			flying = true
+		return
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -25,4 +27,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	move_and_slide()
+	move_and_collide(velocity)
