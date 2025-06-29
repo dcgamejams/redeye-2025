4dfefd8b633ff262d3707db89723d9b3c8e6bbcb
Andrew Davis <jonandrewdavis@gmail.com>
2025-06-29 13:14:29 -0400

    feat: add polygon and ui tweaks

--- scenes/player_tenticle/tenticle.gd
+++ scenes/player_tenticle/tenticle.gd
@@ -2,11 +2,13 @@ class_name Tenticle
 extends Node3D
 
 @onready var path: Path3D = %"Path"
+@onready var tenticle_poly: CSGPolygon3D = %CSGPolygon3D
 
 var offset = Vector3(0, 0.2, 0)
 
 func _ready():
 	clear()
+	tenticle_poly.polygon = generate_circle_polygon(0.23, 64, Vector2.ZERO)
 
 func add_point(point: Vector3):
 	point -= offset
@@ -23,3 +25,15 @@ func pop() -> Vector3:
 
 func clear():
 	path.curve.clear_points()
+
+
+func generate_circle_polygon(radius: float, num_sides: int, position: Vector2) -> PackedVector2Array:
+	var angle_delta: float = (PI * 2) / num_sides
+	var vector: Vector2 = Vector2(radius, 0)
+	var polygon: PackedVector2Array
+
+	for _i in num_sides:
+		polygon.append(vector + position)
+		vector = vector.rotated(angle_delta)
+
+	return polygon
